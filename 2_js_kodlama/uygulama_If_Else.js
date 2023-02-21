8dd6c0b9348e8d96c394eb08844778abd95ffb62
NaimeDmts <naime.dmts@gmail.com>
2023-02-21 16:36:58 +0300

    11-uygulama If/Else-2

--- 2_js_kodlama/uygulama_If_Else.js
+++ 2_js_kodlama/uygulama_If_Else.js
@@ -1,6 +1,6 @@
 // 1- Bir sayının 10-50 arasında olup olmadığını kontrol ediniz.
 
-let sayi = 20;
+let sayi = 21;
 if(sayi>10 && sayi<50){
     console.log("Sayı 10-50 arasındadır.");
 }
@@ -8,13 +8,13 @@ else{
     console.log("Sayı 10-50 arasında değildir.");
 }
 
-// 2- Bir sayının pozitif tek sayı olup olmadığını kontrol ediniz.
+// 2- Bir sayının pozitiftek sayı olup olmadığını kontrol ediniz.
 
-if(sayi > 0){
-    console.log("Sayı pozitiftir.");
+if(sayi > 0 && sayi%2 == 1){
+    console.log("Sayı pozitiftir tek sayıdır");
 }
 else{
-    console.log("Sayı negatiftir.");
+    console.log("Sayı pozitif çift sayıdır");
 }
 
 // 3- x, y, z sayılarının büyüklük karşılaştırmasını yapınız.
@@ -30,6 +30,9 @@ else if(y > x && y > z ){
 else if(z > x && z > y){
     console.log("z en büyük sayı");
 }
+else{
+    console.log("Sayılar eşit");
+}
 
 // 4- 2 vize ve 1 final notuna göre hesaplanan ortalama için;
     // a- Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazsın.
@@ -37,9 +40,9 @@ else if(z > x && z > y){
     // c-Finalden 70 alındığında ortalama 50'nin altında  olsa bile dersten geçilsin.
 
 let vize1 = 70, vize2 = 65, final = 50, 
-ortalama = (vize1 + vize2 + final)/3;
-
-if((ortalama >=50 && final >= 50)|| final == 70){
+ortalama = (((vize1 + vize2)/2)*0.4) + final*0.6;
+console.log("Ortalamanız:" + ortalama);
+if((ortalama >=50 && final >= 50)|| final >= 70){
     console.log("Geçti");
 }
 else{
