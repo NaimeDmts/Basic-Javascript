7c39d0a4f03dcd8700cdb8e47042ef98825013b9
NaimeDmts <naime.dmts@gmail.com>
2023-02-21 15:03:21 +0300

    9-If/Else Koşul İfadeleri-1

---
+++ 2_js_kodlama/If_Else_Kosul_Ifadeleri_1.js
@@ -0,0 +1,35 @@
+// let isLoggedin = true;
+let username = "NaimeDmts";
+let password = "1234";
+let isLoggedin = (username == "Naime");
+
+if (username == "NaimeDmts"){
+    if(password == "1234"){
+        console.log("uygulamaya giriş yapıldı.");
+    }
+    else{
+        console.log("Parola Yanlış.");
+    }
+}
+else{
+    console.log("Kullanıcı adı yanlış.");
+}
+
+if(3 > 4)
+{
+    console.log("sayı büyük");
+}
+else
+{
+    console.log("sayı küçük");
+}
+
+if(isLoggedin)
+{
+    console.log("Giriş yapıldı");
+}
+else{
+    console.log("Giriş yapılmadı");
+}
+
+
