67dd49ce75defd130ad80c06ff93d1d1c07329e5
NaimeDmts <naime.dmts@gmail.com>
2023-02-21 14:41:34 +0300

    8-Operatörler

---
+++ 2_js_kodlama/operatorler.js
@@ -0,0 +1,33 @@
+let sonuc;
+let a = 10, b = 20, c = 30;
+// 1- Aritmetik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = c % a;
+sonuc = a++;
+sonuc = ++a;
+sonuc = a--;
+sonuc = --a;
+
+// 2- Atama Operatörleri
+sonuc = a;
+sonuc += a;
+sonuc +=(a+b);
+sonuc -= a;
+sonuc *= a;
+sonuc /= a;
+sonuc %= a;
+
+// 3- Karşılaştırma Operatörleri
+sonuc = (a == b);
+sonuc = (a != b);
+sonuc = (3 === "3")// değer ve tip kontrolü
+sonuc = (a > b)
+sonuc = (a < b)
+sonuc = (a <= b)
+
+// 4- Mantıksal Operatörler
+
+ console.log(sonuc);
