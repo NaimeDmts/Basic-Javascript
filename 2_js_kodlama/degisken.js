15ab648c21036a20998f666b0a34f422fc3d78c2
NaimeDmts <naime.dmts@gmail.com>
2023-02-21 13:29:15 +0300

    6-Degiskenler

---
+++ 2_js_kodlama/degisken.js
@@ -0,0 +1,55 @@
+//4-Javascript Yazmaya Başlayalım!
+console.log("merhaba")
+//5- Değişkenler
+console.log(5000 + (5000 * 0.3)) //maaş ali
+console.log(6000 + (6000 * 0.3)) //maaş veli
+
+var maasAli = 5000;
+var maasVeli = 6000;
+console.log(maasAli + (maasAli * 0.3))
+console.log(maasVeli + (maasVeli * 0.3))
+
+var zam = 0.3;
+console.log(maasAli + (maasAli * zam))
+console.log(maasVeli + (maasVeli * zam))
+
+//6-Değişken Türleri
+var urunAdi = "iphone 13"; //string
+let urunFiyat = 15000; //number
+
+console.log(typeof urunAdi)
+console.log(typeof urunFiyat)
+
+let sayi1 = "10";
+let sayi2 = "20";
+console.log(sayi1 + sayi2)
+
+let sayi3 = 10;
+let sayi4 = 20;
+console.log(sayi3 + sayi4)
+
+console.log(Number(sayi1)+Number(sayi2));
+console.log(sayi3.toString() + sayi4.toString());
+
+let isim = "Naime";
+let soyad = "Damataşı";
+console.log(isim + soyad);
+console.log(isim + " " + soyad);
+
+let sinavNotu1 = 70;
+let basarilimi1 = (sinavNotu1 >= 50);
+console.log(basarilimi1);
+
+let sinavNotu2 = 40;
+let basarilimi2 = (sinavNotu2 >= 50);
+console.log(basarilimi2);
+
+console.log(typeof basarilimi1);
+
+let yas;
+console.log(yas);
+console.log(typeof yas);
+
+yas="";
+console.log(yas);
+console.log(typeof yas);
